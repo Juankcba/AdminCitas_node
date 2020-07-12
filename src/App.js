5c70f993f602f0c90bee25c10cf7af32cfd81435
Juan Carlos Gonzalez <juangblade@hotmail.com>
2020-07-12 08:07:05 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment,useState,useEffect} from 'react';
+import Formulario from './components/Formulario'
+import Cita from './components/Cita'
 
 function App() {
+
+  //citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+  //Arreglo de citas
+  const [citas, guardarCita] = useState(citasIniciales);
+
+  //Use effect para rea     lizar ciestas operaciones cuando el state cambia
+  useEffect( () => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+    if(citasIniciales)
+    {
+      localStorage.setItem('citas',JSON.stringify(citas));
+    } else
+    {
+      localStorage.setItem('citas',JSON.stringify([]));
+    }
+  },[citas]);
+  //Funcion que actualiza las cita
+
+  const crearCita = cita => {
+    guardarCita([
+      ...citas,
+      cita
+    ]);
+  }
+
+  //Funcion que elimina una cita por su ID
+  const eliminarCita = id =>{
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCita(nuevasCitas);
+  }
+
+  //Mensaje condicional
+
+  const titulo = citas.length === 0 ? 'No Hay Citas': 'Administra tus Citas'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+
+      <h1>Administrador de Pacientes</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+              <Formulario  
+                 crearCita={crearCita}
+              />
+          </div>
+          <div className="one-half column">
+              <h2> {titulo}</h2>
+              {citas.map(cita => (
+                <Cita
+                key={cita.id}
+                  cita={cita}
+                  eliminarCita={eliminarCita}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
